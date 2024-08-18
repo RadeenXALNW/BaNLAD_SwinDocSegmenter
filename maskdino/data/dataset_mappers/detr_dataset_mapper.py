c1d07c625ae89447a8588867dbb7390e687194b6
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2024-08-19 04:39:30 +0600

    Update detr_dataset_mapper.py for  BaNLAD

--- maskdino/data/dataset_mappers/detr_dataset_mapper.py
+++ maskdino/data/dataset_mappers/detr_dataset_mapper.py
@@ -56,90 +56,68 @@ def build_transform_gen(cfg, is_train):
 
 class DetrDatasetMapper:
     """
-    A callable which takes a dataset dict in Detectron2 Dataset format,
-    and map it into a format used by DETR.
-
-    The callable currently does the following:
-
-    1. Read the image from "file_name"
-    2. Applies geometric transforms to the image and annotation
-    3. Find and applies suitable cropping to the image and annotation
-    4. Prepare image and annotation to Tensors
+    A callable which takes a dataset dict in Detectron2 Dataset format
+    and maps it into a format used by a custom model.
     """
 
     def __init__(self, cfg, is_train=True):
         if cfg.INPUT.CROP.ENABLED and is_train:
             self.crop_gen = [
-                T.Resize((512,512),
+                T.Resize((512, 512)),
                 T.ResizeShortestEdge([400, 500, 600], sample_style="choice"),
                 T.RandomCrop(cfg.INPUT.CROP.TYPE, cfg.INPUT.CROP.SIZE),
             ]
         else:
-            self.crop_gen = None
-
-        self.mask_on = True
-        self.tfm_gens = build_transform_gen(cfg, is_train)
-        logging.getLogger(__name__).info(
-            "Full TransformGens used in training: {}, crop: {}".format(str(self.tfm_gens), str(self.crop_gen))
-        )
+            self.crop_gen = [T.Resize((512, 512)),]
 
         self.img_format = cfg.INPUT.FORMAT
         self.is_train = is_train
+        self.transform_list = [
+            T.RandomBrightness(0.9, 1.0),
+            T.RandomContrast(0.8, 1.4),
+            T.Resize((800, 800)),
+        ]
+
+        logging.getLogger(__name__).info(
+            "Transformations used: {}".format(str(self.transform_list))
+        )
 
     def __call__(self, dataset_dict):
         """
         Args:
             dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
-
         Returns:
             dict: a format that builtin models in detectron2 accept
         """
-        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        dataset_dict = copy.deepcopy(dataset_dict)  # It will be modified by code below
         image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
+        
         utils.check_image_size(dataset_dict, image)
 
-        if self.crop_gen is None:
-            image, transforms = T.apply_transform_gens(self.tfm_gens, image)
-        else:
-            if np.random.rand() > 0.5:
-                image, transforms = T.apply_transform_gens(self.tfm_gens, image)
-            else:
-                image, transforms = T.apply_transform_gens(
-                    self.tfm_gens[:-1] + self.crop_gen + self.tfm_gens[-1:], image
-                )
+        # Apply transformations
+        image, image_transform = T.apply_transform_gens(self.transform_list, image)
 
-        image_shape = image.shape[:2]  # h, w
-
-        # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
-        # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
-        # Therefore it's important to use torch.Tensor.
-        dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose(2, 0, 1)))
-
-        if not self.is_train:
-            # USER: Modify this if you want to keep them for some reason.
-            dataset_dict.pop("annotations", None)
-            return dataset_dict
+        # Convert the image to a tensor
+        dataset_dict["image"] = torch.as_tensor(
+            np.ascontiguousarray(image.transpose(2, 0, 1)).astype("float32")
+        )
 
         if "annotations" in dataset_dict:
-            # USER: Modify this if you want to keep them for some reason.
-            for anno in dataset_dict["annotations"]:
-                if not self.mask_on:
-                    anno.pop("segmentation", None)
-                anno.pop("keypoints", None)
-
-            # USER: Implement additional transformations if you have other types of data
+            # Transform annotations
             annos = [
-                utils.transform_instance_annotations(obj, transforms, image_shape)
+                utils.transform_instance_annotations(obj, image_transform, image.shape[:2])
                 for obj in dataset_dict.pop("annotations")
                 if obj.get("iscrowd", 0) == 0
             ]
-            instances = utils.annotations_to_instances(annos, image_shape)
+            instances = utils.annotations_to_instances(annos, image.shape[:2])
             instances = utils.filter_empty_instances(instances)
-            h, w = instances.image_size
+            
             if hasattr(instances, 'gt_masks'):
                 gt_masks = instances.gt_masks
+                h, w = image.shape[:2]  # Use the transformed image size
                 gt_masks = convert_coco_poly_to_mask(gt_masks.polygons, h, w)
                 instances.gt_masks = gt_masks
 
             dataset_dict["instances"] = instances
+
         return dataset_dict
