7a211a4640b7398a68503740a08c4f3e40a2c25b
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2024-08-19 04:47:55 +0600

    Update train_net.py for BaNLAD

--- train_net.py
+++ train_net.py
@@ -45,7 +45,7 @@ from detectron2.utils.logger import setup_logger
 
 from detectron2.data.datasets import register_coco_instances
 from detectron2.data.datasets.coco import convert_to_coco_json
-
+#change
 # MaskDINO
 from maskdino import (
     COCOInstanceNewBaselineDatasetMapper,
